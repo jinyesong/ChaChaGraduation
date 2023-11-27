f4b2961bbd0e871c7f01cfe90680ed5f5995be22
Yejin Song <songyejin2000@gmail.com>
2023-11-27 12:21:45 +0900

    refactoring

--- src/source/GameManager.java
+++ src/source/GameManager.java
@@ -1,6 +1,27 @@
 package source;
 
 public class GameManager { 
+	Player player;
+	UI ui;
+	
+	public void startGame(Player player) {
+		//loadGame에서 호출?
+		this.player = player;
+		//메인 UI 불러와서 setting
+	}
+    public void endGame() {
+    	//졸업 엔딩 확인
+    	String ending = player.checkEndingConditions();
+    	//Ending 함수 호출
+    }
+    public void seasonChange() {
+    	//TimeManager에서 호출?
+    	//UI 변경
+    	//이벤트 처리
+    	//레벨 변경 -> 장학금 여부
+    }
+    
+    
     public GameManager(Player player , UI ui) {
         this.player = player;
         this.ui = ui;
