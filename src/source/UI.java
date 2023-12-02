c7eb178ef980e03b809fd1efb7fb1136c5750e72
sans29374 <143861493+sans29374@users.noreply.github.com>
2023-12-02 12:26:49 +0900

    Update UI.java

--- src/source/UI.java
+++ src/source/UI.java
@@ -11,39 +11,45 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class UI extends JFrame {
 	final int Main_WIDTH = 550;
 	final int Main_HEIGHT = 700;
 
-	String season; // 계절 이름
-	int grade; // 학년
-	int money; // 돈
-	int clicks; // 클릭수(계절 당 클릭수를 경험치처럼 표현하기 위함)
-	int knowledge; // 지식 스탯
-	int happiness; // 행복 스탯
+	Stat statPanel;
+	Below belowPanel;
+
+	Player player;
+	int season; // 계절 1: Spring, 2: Summer, 3: Fall, 4: Winter
+//	int grade; // 학년
+//	int money; // 돈
+//	int clicks; // 클릭수(계절 당 클릭수를 경험치처럼 표현하기 위함)
+//	int knowledge; // 지식 스탯
+//	int happiness; // 행복 스탯
 
 	public UI(Player player) {
 		setSize(Main_WIDTH+14, Main_HEIGHT+37);
 		setLayout(null);
 
-		season = "Spring"; // 초기 계절 = 봄
-		grade = player.getLevel(); // 초기 학년 = 1학년
-		money = player.getMoney(); // 초기 돈 액수 = 0원
-		clicks = 0; // 초기 클릭수 = 0
-		knowledge = player.getKnowledge(); // 초기 지식 스탯
-		happiness = player.getHappiness(); // 초기 행복 스탯
+		this.player = player;
+		season = player.timeManager.getSeason(); // 초기 계절 = 봄
+//		grade = player.getLevel(); // 초기 학년 = 1학년
+//		money = player.getMoney(); // 초기 돈 액수 = 0원
+//		clicks = 0; // 초기 클릭수 = 0
+//		knowledge = player.getKnowledge(); // 초기 지식 스탯
+//		happiness = player.getHappiness(); // 초기 행복 스탯
 
 		ChaCha chaChaPanel = new ChaCha();
 		chaChaPanel.setBounds(0, 0, Main_WIDTH, Main_HEIGHT);
 		add(chaChaPanel);
 
-		Stat statPanel = new Stat();
+		statPanel = new Stat();
 		statPanel.setBounds(0, 0, Main_WIDTH, 100); // ChaCha 패널과 크기를 맞춤
 		chaChaPanel.add(statPanel); // ChaCha 패널에 Stat 패널 추가
 
-		Below belowPanel = new Below();
+		belowPanel = new Below();
 		belowPanel.setBounds(0, 470, Main_WIDTH, Main_HEIGHT);
 		chaChaPanel.add(belowPanel);
 
@@ -51,33 +57,59 @@ public class UI extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
+	public void clickCheck() {
+		// player의 click수 업데이트
+		player.timeManager.addClickCount();
+		// click수에 따른 이벤트 처리
+
+		// player 정보 update되어 repaint
+		statPanel.repaint(); // 액션 스탯 업데이트
+		belowPanel.repaint(); // 클릭 스탯 업데이트
+	}
+
 	class ChaCha extends JPanel {
 		JLabel gradeLabel;
+		ImageIcon logoutIcon, saveIcon;
+		JButton logoutButton, saveButton;
 
 		public ChaCha() {
 			setSize(550, 700);
 			setLayout(null);
 
-			//학년 label 추가하기
-			gradeLabel = new JLabel(Integer.toString(grade));
+			// 학년 label 추가하기
+			gradeLabel = new JLabel(Integer.toString(player.getLevel()));
 			gradeLabel.setFont(new Font("Serif", Font.BOLD, 25));
 			gradeLabel.setHorizontalAlignment(JLabel.CENTER);
 			gradeLabel.setVerticalAlignment(JLabel.CENTER);
 			add(gradeLabel);
 
-			//저장, 로그아웃 버튼 추가하기
+			// 저장, 로그아웃 버튼 추가하기
 			logoutIcon = new ImageIcon("src/img/logout_button.png");
 			logoutButton = new JButton(logoutIcon);
 			saveIcon = new ImageIcon("src/img/save_button.png");
 			saveButton = new JButton(saveIcon);
-			
+
 			for (JButton button : new JButton[] { saveButton, logoutButton }) {
 				button.setBorderPainted(false);
 				button.setFocusPainted(false);
 				button.setContentAreaFilled(false);
-				
+
 				add(button);
 			}
+
+			saveButton.addActionListener(e -> {
+				FileIO fi = new FileIO();
+				fi.saveGame(player);
+				JOptionPane.showMessageDialog(null, "저장 완료!.");
+			});
+			logoutButton.addActionListener(e -> {
+				int result = 0;
+				result = JOptionPane.showConfirmDialog(null, "로그아웃 하시겠습니까?");
+				if (result == 0) { // 로그아웃
+					dispose(); // 현재 윈도우가 닫힘
+					new LoginFrame();
+				}
+			});
 		}
 
 		@Override
@@ -154,27 +186,24 @@ public class UI extends JFrame {
 			g.setColor(Color.BLACK);
 			g.drawRect(season_frameX, season_frameY, season_frameWidth, season_frameHeight);
 
-			//저장, 로그아웃 버튼 위치시키기
-			saveButton.setBounds(getWidth()-saveIcon.getIconWidth()-5, centerY+35-saveIcon.getIconHeight(), saveIcon.getIconWidth(), saveIcon.getIconHeight());
-			logoutButton.setBounds(getWidth()-saveIcon.getIconWidth()-5, centerY+35, logoutIcon.getIconWidth(), logoutIcon.getIconHeight());
+			// 저장, 로그아웃 버튼 위치시키기
+			saveButton.setBounds(getWidth() - saveIcon.getIconWidth() - 5, centerY + 35 - saveIcon.getIconHeight(),
+					saveIcon.getIconWidth(), saveIcon.getIconHeight());
+			logoutButton.setBounds(getWidth() - saveIcon.getIconWidth() - 5, centerY + 35, logoutIcon.getIconWidth(),
+					logoutIcon.getIconHeight());
 		}
 
-		private ImageIcon getFrameIconForSeason(String season) {
-			if (season != null) {
-				switch (season) {
-				case "Spring":
-					return new ImageIcon("path_to_spring_icon.png");
-				case "Summer":
-					return new ImageIcon("path_to_summer_icon.png");
-				case "Autumn":
-					return new ImageIcon("path_to_autumn_icon.png");
-				case "Winter":
-					return new ImageIcon("path_to_winter_icon.png");
-				default:
-					return new ImageIcon("path_to_default_icon.png");
-				}
-			} else {
-				// season이 null인 경우에 대한 처리
+		private ImageIcon getFrameIconForSeason(int season) {
+			switch (season) {
+			case 1:
+				return new ImageIcon("path_to_spring_icon.png");
+			case 2:
+				return new ImageIcon("path_to_summer_icon.png");
+			case 3:
+				return new ImageIcon("path_to_autumn_icon.png");
+			case 4:
+				return new ImageIcon("path_to_winter_icon.png");
+			default:
 				return new ImageIcon("path_to_default_icon.png");
 			}
 		}
@@ -186,7 +215,7 @@ public class UI extends JFrame {
 
 		public Stat() {
 			// money 레이블 초기화 및 설정
-			moneyLabel = new JLabel("" + money);
+			moneyLabel = new JLabel();
 			moneyLabel.setFont(new Font("Serif", Font.BOLD, 25));
 			moneyLabel.setHorizontalAlignment(JLabel.CENTER);
 			moneyLabel.setVerticalAlignment(JLabel.CENTER);
@@ -204,7 +233,7 @@ public class UI extends JFrame {
 			// 이미지 아이콘 로드
 			ImageIcon moneyIcon = new ImageIcon("src/img/money.png");
 			ImageIcon knowledgeIcon = new ImageIcon("src/img/knowledge.png");
-			ImageIcon happinessIcon = new ImageIcon("src/img/Chappiness.png");
+			ImageIcon happinessIcon = new ImageIcon("src/img/happiness.png");
 
 			// 간격 설정
 			int gap = 10;
@@ -214,47 +243,42 @@ public class UI extends JFrame {
 			int rectangleWidth = 2 * iconWidth + 3;
 
 			// 돈 아이콘 그리기
-			moneyIcon.paintIcon(this, g, 0, y);
+			moneyIcon.paintIcon(this, g, gap, y);
 			gap += iconWidth;
 
 			// 돈 스탯 표시하는 직사각형 그리기
 			g.setColor(Color.white);
-			g.fillRoundRect(gap - 13, y + 6, gap + iconWidth - 10, iconHeight - 10, 10, 10);
+			g.fillRoundRect(gap, y + 6, gap + iconWidth - 10, iconHeight - 10, 10, 10);
 			g.setColor(Color.black);
-			g.drawRoundRect(gap - 13, y + 6, gap + iconWidth - 10, iconHeight - 10, 10, 10);
-
-			// 돈 스탯 표시하는 직사각형 그리기
-			g.setColor(Color.white);
-			g.fillRoundRect(gap - 13, y + 6, gap + iconWidth - 10, iconHeight - 10, 10, 10);
-			g.setColor(Color.black);
-			g.drawRoundRect(gap - 13, y + 6, gap + iconWidth - 10, iconHeight - 10, 10, 10);
+			g.drawRoundRect(gap, y + 6, gap + iconWidth - 10, iconHeight - 10, 10, 10);
 
 			// 돈 레이블 위치 설정
-			moneyLabel.setBounds(gap - 13, y + 6, gap + iconWidth - 10, iconHeight - 10);
+			moneyLabel.setBounds(gap, y + 6, gap + iconWidth - 10, iconHeight - 10);
+			moneyLabel.setText(Integer.toString(player.getMoney()));
 
 			gap += gap + iconWidth;
 
 			// 지식 아이콘 그리기
-			knowledgeIcon.paintIcon(this, g, gap - 16, y);
+			knowledgeIcon.paintIcon(this, g, gap-4, y);
 			gap += iconWidth;
 
 			// 지식 스탯 표시하는 직사각형 그리기
-			drawStatRectangle(g, knowledge, gap - 13, y + 6, rectangleWidth, iconHeight - 10);
+			drawStatRectangle(g, player.getKnowledge(), gap, y + 6, rectangleWidth, iconHeight - 10);
 			gap += 120;
 
 			// 행복도 아이콘 그리기
-			happinessIcon.paintIcon(this, g, gap - 16, y);
+			happinessIcon.paintIcon(this, g, gap, y);
 			gap += iconWidth;
 
 			// 행복도 스탯 표시하는 직사각형 그리기
-			drawStatRectangle(g, happiness, gap - 13, y + 6, rectangleWidth, iconHeight - 10);
+			drawStatRectangle(g, player.getHappiness(), gap, y + 6, rectangleWidth, iconHeight - 10);
 		}
 
 		// 스탯에 따라 직사각형 색깔 채우기
 		private void drawStatRectangle(Graphics g, int value, int x, int y, int width, int height) {
 			// 스탯의 백분율에 따라 채워지는 너비 계산
 			int filledWidth = width * value / 100;
-
+			
 			// 직사각형 그리기
 			g.setColor(new Color(0, 160, 186));
 			g.fillRoundRect(x, y, filledWidth, height, 10, 10);
@@ -265,20 +289,19 @@ public class UI extends JFrame {
 	}
 
 	class Below extends JPanel {
-		JLabel study_label, work_label, sleep_label, eat_label, play_label;
 		JButton studyButton, workButton, sleepButton, eatButton, playButton;
-		//Action eventListener = new Action();
+		
+		// 차차 액션 아이콘 불러오기
+		ImageIcon studyIcon = new ImageIcon("src/img/study.png");
+		ImageIcon workIcon = new ImageIcon("src/img/work.png");
+		ImageIcon sleepIcon = new ImageIcon("src/img/sleep.png");
+		ImageIcon eatIcon = new ImageIcon("src/img/eat.png");
+		ImageIcon playIcon = new ImageIcon("src/img/play.png");
 
 		public Below() {
 			setLayout(null);
-
-			// 차차 액션 아이콘 및 버튼 불러오기
-			ImageIcon studyIcon = new ImageIcon("src/img/study.png");
-			ImageIcon workIcon = new ImageIcon("src/img/work.png");
-			ImageIcon sleepIcon = new ImageIcon("src/img/sleep.png");
-			ImageIcon eatIcon = new ImageIcon("src/img/eat.png");
-			ImageIcon playIcon = new ImageIcon("src/img/play.png");
-
+			
+			//차차 버튼 불러오기
 			studyButton = new JButton(new ImageIcon("src/img/study_button.png"));
 			workButton = new JButton(new ImageIcon("src/img/work_button.png"));
 			sleepButton = new JButton(new ImageIcon("src/img/sleep_button.png"));
@@ -286,60 +309,31 @@ public class UI extends JFrame {
 			playButton = new JButton(new ImageIcon("src/img/play_button.png"));
 
 			// 각 버튼에 액션 이벤트 리스너 추가
-//			studyButton.addActionListener(eventListener);
-//			workButton.addActionListener(eventListener);
-//			sleepButton.addActionListener(eventListener);
-//			eatButton.addActionListener(eventListener);
-//			playButton.addActionListener(eventListener);
+			studyButton.addActionListener(e -> {
+				player.setKnowledge(player.getKnowledge() + 10);
+				player.setHappiness(player.getHappiness() - 5);
+				clickCheck();
+			});
+			workButton.addActionListener(e -> {
+				player.setMoney(player.getMoney() + 20);
+				clickCheck();
+			});
+			sleepButton.addActionListener(e -> {
+				player.setHappiness(player.getHappiness() + 15);
+				clickCheck();
+			});
+			eatButton.addActionListener(e -> {
+				player.setHappiness(player.getHappiness() + 20);
+				player.setMoney(player.getMoney() - 10);
+				clickCheck();
+			});
 
-			study_label = new JLabel();
-			work_label = new JLabel();
-			sleep_label = new JLabel();
-			eat_label = new JLabel();
-			play_label = new JLabel();
-
-			// 이미지 레이블 등록하기
-			study_label.setIcon(studyIcon);
-			work_label.setIcon(workIcon);
-			sleep_label.setIcon(sleepIcon);
-			eat_label.setIcon(eatIcon);
-			play_label.setIcon(playIcon);
-
-			// 각 버튼 및 차차 액션 아이콘 위치 설정
-			int icon_gap = 7;
-			int button_gap = 23;
-			int y = 10;
-			int iconWidth = studyIcon.getIconWidth();
-			int iconHeight = studyIcon.getIconHeight();
-			int buttonWidth = studyButton.getIcon().getIconWidth();
-			int buttonHeight = studyButton.getIcon().getIconHeight();
-
-			study_label.setBounds(icon_gap, y, iconWidth, iconHeight);
-			studyButton.setBounds(button_gap, y + iconHeight, buttonWidth, buttonHeight);
-
-			icon_gap += iconWidth + 10;
-			button_gap += buttonWidth + 13;
-
-			work_label.setBounds(icon_gap - 30, y + 5, iconWidth, iconHeight);
-			workButton.setBounds(button_gap, y + iconHeight, buttonWidth, buttonHeight);
-
-			icon_gap += iconWidth + 5;
-			button_gap += buttonWidth + 13;
-
-			sleep_label.setBounds(icon_gap - 45, y - 5, iconWidth, iconHeight);
-			sleepButton.setBounds(button_gap, y + iconHeight, buttonWidth, buttonHeight);
-
-			icon_gap += iconWidth + 5;
-			button_gap += buttonWidth + 13;
-
-			eat_label.setBounds(icon_gap - 80, y, iconWidth, iconHeight);
-			eatButton.setBounds(button_gap, y + iconHeight, buttonWidth, buttonHeight);
-
-			icon_gap += iconWidth + 5;
-			button_gap += buttonWidth + 13;
-
-			play_label.setBounds(icon_gap - 90, y, iconWidth, iconHeight);
-			playButton.setBounds(button_gap, y + iconHeight, buttonWidth, buttonHeight);
+			playButton.addActionListener(e -> {
+				player.setKnowledge(player.getKnowledge() - 10);
+				player.setHappiness(player.getHappiness() + 20);
+				player.setMoney(player.getMoney() - 15);
+				clickCheck();
+			});
 
 			for (JButton button : new JButton[] { studyButton, workButton, sleepButton, eatButton, playButton }) {
 				button.setBorderPainted(false);
@@ -348,18 +342,49 @@ public class UI extends JFrame {
 				add(button);
 			}
 
-			add(study_label);
-			add(work_label);
-			add(sleep_label);
-			add(eat_label);
-			add(play_label);
-
-			setOpaque(true); // 패널 배경을 투명하게 설정
+			setOpaque(false); // 패널 배경을 투명하게 설정
 		}
 
 		@Override
 		protected void paintComponent(Graphics g) {
-			drawClickRectangle(g, clicks, 0, getHeight() - 30, getWidth(), getHeight());
+
+			// 각 버튼 및 차차 액션 아이콘 위치 설정
+			int icon_gap = 25;
+			int button_gap = 23;
+			int y = 10;
+			int iconWidth = studyIcon.getIconWidth();
+			int iconHeight = studyIcon.getIconHeight();
+			int buttonWidth = studyButton.getIcon().getIconWidth();
+			int buttonHeight = studyButton.getIcon().getIconHeight()+70;
+
+			studyIcon.paintIcon(this, g ,icon_gap, y+35);
+			studyButton.setBounds(button_gap, y + iconHeight, buttonWidth, buttonHeight);
+
+			icon_gap += iconWidth;
+			button_gap += buttonWidth + 13;
+
+			workIcon.paintIcon(this, g, icon_gap, y+35);
+			workButton.setBounds(button_gap, y + iconHeight, buttonWidth, buttonHeight);
+
+			icon_gap += iconWidth + 28;
+			button_gap += buttonWidth + 13;
+
+			sleepIcon.paintIcon(this, g, icon_gap, y+13);
+			sleepButton.setBounds(button_gap, y + iconHeight, buttonWidth, buttonHeight);
+
+			icon_gap += iconWidth + 13;
+			button_gap += buttonWidth + 13;
+
+			eatIcon.paintIcon(this, g, icon_gap, y+12);
+			eatButton.setBounds(button_gap, y + iconHeight, buttonWidth, buttonHeight);
+
+			icon_gap += iconWidth + 24;
+			button_gap += buttonWidth + 13;
+
+			playIcon.paintIcon(this, g, icon_gap, y+25);
+			playButton.setBounds(button_gap, y + iconHeight, buttonWidth, buttonHeight);
+
+			drawClickRectangle(g, player.timeManager.getClickCount(), 0, getHeight() - 30, getWidth(), getHeight());
 		}
 
 		// 클릭수에 따른 직사각형 모양 변화
