3419127301fb48c8b85164a3f46b729a891b92f8
Yejin Song <songyejin2000@gmail.com>
2023-12-02 10:09:53 +0900

    To be deleted class

--- src/source/GameManager.java
+++ src/source/GameManager.java
@@ -1,54 +1,54 @@
 package source;
 
 public class GameManager { 
-	Player player;
-	UI ui;
-	
-	public void startGame(Player player) {
-		//loadGame에서 호출?
-		this.player = player;
-		//메인 UI 불러와서 setting
-	}
-    public void endGame() {
-    	//졸업 엔딩 확인
-    	String ending = player.checkEndingConditions();
-    	//Ending 함수 호출
-    }
-    public void seasonChange() {
-    	//TimeManager에서 호출?
-    	//UI 변경
-    	//이벤트 처리
-    	//레벨 변경 -> 장학금 여부
-    }
-    
-    
-    public GameManager(Player player , UI ui) {
-        this.player = player;
-        this.ui = ui;
-
-        Buttons(); //버튼을 정의 후
-        checkHappiness(); //버튼이 눌릴 때마다 행복도 체크
-    }
-
-    public void Buttons() {
-        //studyButton.addActionListener(e -> performActionAndCheckHappiness(new StudyAction()));
-        //workButton.addActionListener(e -> performActionAndCheckHappiness(new WorkAction()));
-        //sleepButton.addActionListener(e -> performActionAndCheckHappiness(new SleepAction()));
-        //eatButton.addActionListener(e -> performActionAndCheckHappiness(new EatAction()));
-        //playButton.addActionListener(e -> performActionAndCheckHappiness(new HaveFunAction()));
-    }
-
-    public void performActionAndCheckHappiness(Action action) {
-        
-        checkHappiness();
-        //클릭에 따른 ui 및 수치 변경
-    }
-
-    public void checkHappiness() {
-        if (player.getHappiness() <= 30) {
-            //행복도가 30 이하일 때 자퇴엔딩
-        }
-    }
+//	Player player;
+//	UI ui;
+//	
+//	//클릭마다 행복도 체크해서 자퇴 엔딩
+//	//클릭마다 계절 확인해서 UI변경, 특정 시기에 event 발생 컨트롤
+//	
+//	public GameManager(Player player) {
+//        this.player = player;
+//        this.ui = new UI(player);
+//    }
+//	
+//	public void startGame(Player player) {
+//		//loadGame에서 호출?
+//		this.player = player;
+//		//메인 UI 불러와서 setting
+//	}
+//    public void endGame() {
+//    	//졸업 엔딩 확인
+//    	String ending = player.checkEndingConditions();
+//    	//Ending 함수 호출
+//    }
+//    public void seasonChange() {
+//    	//TimeManager에서 호출?
+//    	//UI 변경
+//    	//이벤트 처리
+//    	//레벨 변경 -> 장학금 여부
+//    }
+//    
+//
+//    public void Buttons() {
+//        //studyButton.addActionListener(e -> performActionAndCheckHappiness(new StudyAction()));
+//        //workButton.addActionListener(e -> performActionAndCheckHappiness(new WorkAction()));
+//        //sleepButton.addActionListener(e -> performActionAndCheckHappiness(new SleepAction()));
+//        //eatButton.addActionListener(e -> performActionAndCheckHappiness(new EatAction()));
+//        //playButton.addActionListener(e -> performActionAndCheckHappiness(new HaveFunAction()));
+//    }
+//
+//    public void performActionAndCheckHappiness(Action action) {
+//        
+//        checkHappiness();
+//        //클릭에 따른 ui 및 수치 변경
+//    }
+//
+//    public void checkHappiness() {
+//        if (player.getHappiness() <= 30) {
+//            //행복도가 30 이하일 때 자퇴엔딩
+//        }
+//    }
 }
 
 //Game 클래스 와의 차이점을 구별해서 더 작성해야 할듯 함
--- src/source/Level.java
+++ src/source/Level.java
@@ -3,35 +3,35 @@ package source;
 import javax.swing.JOptionPane;
 
 class Level {
-    private int currentLevel;
-    private int knowledgeThreshold;
-    private boolean scholarshipAwarded; // 장학금이 지급되었는지 여부를 나타내는 변수
-
-    public Level() {
-        this.currentLevel = 1;
-        this.knowledgeThreshold = 30;
-        this.scholarshipAwarded = false;
-    }
-
-//    public boolean levelUp(int knowledge, ChaChaGraduation chaChaGraduation) {
-//        if (knowledge >= knowledgeThreshold) {
-//            currentLevel++;
-//            knowledgeThreshold += 30;
+//    private int currentLevel;
+//    private int knowledgeThreshold;
+//    private boolean scholarshipAwarded; // 장학금이 지급되었는지 여부를 나타내는 변수
 //
-//            // Knowledge가 300 이상이면 장학금 지급
-//            if (knowledge >= 300 && !scholarshipAwarded) {
-//                JOptionPane.showMessageDialog(chaChaGraduation, "레벨 업 성공! & 장학금 지급!");
-//                chaChaGraduation.addMoney(200);
-//                scholarshipAwarded = true;
-//            }
-//
-//            return true;
-//        } else {
-//            return false;
-//        }
+//    public Level() {
+//        this.currentLevel = 1;
+//        this.knowledgeThreshold = 30;
+//        this.scholarshipAwarded = false;
 //    }
-
-    public int getCurrentLevel() {
-        return currentLevel;
-    }
+//
+////    public boolean levelUp(int knowledge, ChaChaGraduation chaChaGraduation) {
+////        if (knowledge >= knowledgeThreshold) {
+////            currentLevel++;
+////            knowledgeThreshold += 30;
+////
+////            // Knowledge가 300 이상이면 장학금 지급
+////            if (knowledge >= 300 && !scholarshipAwarded) {
+////                JOptionPane.showMessageDialog(chaChaGraduation, "레벨 업 성공! & 장학금 지급!");
+////                chaChaGraduation.addMoney(200);
+////                scholarshipAwarded = true;
+////            }
+////
+////            return true;
+////        } else {
+////            return false;
+////        }
+////    }
+//
+//    public int getCurrentLevel() {
+//        return currentLevel;
+//    }
 }
